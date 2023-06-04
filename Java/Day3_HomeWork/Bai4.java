c0c3db3078cc408f8894454f88a4301a38db1475
Quach Thanh Son <quachthanhson1215@gmail.com>
2023-06-04 14:58:23 +0700

    update

--- Java/Day3_HomeWork/Bai4.java
+++ Java/Day3_HomeWork/Bai4.java
@@ -4,14 +4,12 @@ public class Bai4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Nhập hai số nguyên n và m từ người dùng
         System.out.print("Nhập giá trị của n: ");
         int n = scanner.nextInt();
 
         System.out.print("Nhập giá trị của m: ");
         int m = scanner.nextInt();
 
-        // In hình chữ nhật rỗng
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
