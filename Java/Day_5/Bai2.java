f59bf9d0b80ff106b3ad9846f955bf1a9a2a54a5
Quach Thanh Son <quachthanhson1215@gmail.com>
2023-06-07 20:57:14 +0700

    update

---
+++ Java/Day_5/Bai2.java
@@ -0,0 +1,12 @@
+package Day5;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for(int j = 1; j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
